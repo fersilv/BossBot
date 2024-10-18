a423a65449b037a28a4ab5e275c6567661579582
fersilv <fernandohsilva23@gmail.com>
2024-10-18 16:02:17 -0300

    dfgfedg

--- src/main.ts
+++ src/main.ts
@@ -19,12 +19,20 @@ registerPlugins(app);
 
 app.mount("#app");
 
-// Registrar o Service Worker
-if ("serviceWorker" in navigator) {
+// Função para verificar se é um dispositivo móvel
+function isMobile() {
+  return /Mobi|Android|iPhone|iPad|iPod|Opera Mini/i.test(navigator.userAgent);
+}
+
+// Registrar o Service Worker apenas em dispositivos móveis
+if (isMobile() && "serviceWorker" in navigator) {
   navigator.serviceWorker
     .register("/service-worker.js")
     .then(function (registration) {
-      console.log("Service Worker registrado com sucesso:", registration);
+      console.log(
+        "Service Worker registrado com sucesso em um dispositivo móvel:",
+        registration
+      );
     })
     .catch(function (error) {
       console.error("Falha ao registrar o Service Worker:", error);
